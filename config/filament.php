967e3d15eb55f109bbe9fe62ef88256b65f3026c
Ayatooo <louisreynard919@gmail.com>
2023-03-02 10:30:45 +0100

    :green_heart: filament logo & widget removed

--- config/filament.php
+++ config/filament.php
@@ -25,7 +25,7 @@ return [
     |
     */
 
-    'path' => env('FILAMENT_PATH', 'admin'),
+    'path' => env('FILAMENT_PATH', '/'),
 
     /*
     |--------------------------------------------------------------------------
@@ -140,7 +140,6 @@ return [
         'path' => app_path('Filament/Widgets'),
         'register' => [
             Widgets\AccountWidget::class,
-            Widgets\FilamentInfoWidget::class,
         ],
     ],
 
@@ -236,7 +235,7 @@ return [
             'have_inline_labels' => false,
         ],
         'footer' => [
-            'should_show_logo' => true,
+            'should_show_logo' => false,
         ],
         'max_content_width' => null,
         'notifications' => [
@@ -262,7 +261,7 @@ return [
     |
     */
 
-    'favicon' => null,
+    'favicon' => '/images/logo.png',
 
     /*
     |--------------------------------------------------------------------------
