bda56c13803f8f170c9b5006c9b286e841435b9a
Ayatooo <louisreynard919@gmail.com>
2023-06-10 14:34:29 +0200

    :test_tube: CategoryTest implemented

---
+++ tests/Feature/CategoryTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+uses(TestCase::class);
+
+test('error if not logged in', function () {
+    $response = $this->get('/api/categories');
+    expect($response->status())->toBe(401);
+});
+
+test('show all the existing categories', function () {
+    $user = User::factory()->create();
+    $token = $user->createToken('authToken')->plainTextToken;
+    $response = $this->withHeaders([
+            'Authorization'=>'Bearer '.$token,
+            'Accept' => 'application/json'
+        ])
+        ->get('/api/categories');
+    expect($response->status())->toBe(200);
+});
