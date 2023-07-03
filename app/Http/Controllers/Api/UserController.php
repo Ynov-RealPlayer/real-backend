b567b6715eaaefa55cd419eba9186ec22cb1b2f0
Ayatooo <louisreynard919@gmail.com>
2023-07-03 23:11:46 +0200

    :zap: user->show with rank()

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -31,7 +31,7 @@ class UserController extends Controller
     public function show(User $user) : JsonResponse
     {
         $user = User::where('id', $user->id)
-            ->with('badges', 'medias')
+            ->with('badges', 'medias', 'rank')
             ->first();
         return response()->json($user);
     }
